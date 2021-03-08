51f8a8f2f038a331a5d49935fae28f99c8f550bc
Dotzler <connor@mendingwall.com>
2021-03-07 17:28:24 -0800

    Initial commit

---
+++ Assets/Scripts/Combination.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combination 
+{
+    private List<int> combination;
+    private List<int> defualtCombination = new List<int> { 1, 2, 3, 4 };
+
+    public Combination()
+    {
+        combination = CombinationLoader.Load(defualtCombination);
+    }
+
+    public int GetCombinationDigit(int digitNumber)
+    {
+        if (digitNumber < 0)
+            return 0;
+
+        if (digitNumber >= combination.Count)
+            return 0;
+
+        return combination[digitNumber];
+    }
+
+    public int GetCombinationLength()
+    {
+        return combination.Count;
+    }
+}
